893957220bc7695bb9ad0d3d9981c7a63e76ed69
alca221 <alca_221@hotmail.com>
2023-07-18 20:26:26 -0400

    ver mas

--- views/paginas/servicios.php
+++ views/paginas/servicios.php
@@ -11,7 +11,7 @@
             </picture>
         </div>
 
-        <div class="texto-servicios">
+        <div class="texto-servicios vermas">
             <!-- <blockquote>
             Informáticos
             </blockquote> -->
@@ -22,10 +22,11 @@ Diseño web y desarrollo de páginas web. </p>
             <p>Servicios de soporte técnico para resolver problemas informáticos, tanto en el ámbito de hardware como de software.
 Asistencia remota o presencial para solucionar incidencias y brindar asesoramiento técnico.
 Redes y comunicaciones.</p>
-            <nav class="navegacion">
+<a href="/informatica">Ver mas -></a> 
+            <!-- <nav class="navegacion">
             <a href="/informatica">Ver mas</a> 
                               
-            </nav>
+            </nav> -->
         <!-- <a href="/informatica" class="boton-amarillo-block">
                Ver mas
             </a> -->
@@ -36,7 +37,7 @@ Redes y comunicaciones.</p>
     <h1>Telecomunicaciones y redes</h1>
     </div>
 
-<div class="contenido-servicios">
+<div class="contenido-servicios ">
     <div class="imagen">
         <picture>
             <source srcset="build/img/telecom1.webp" type="image/webp">
@@ -45,17 +46,17 @@ Redes y comunicaciones.</p>
         </picture>
     </div>
 
-    <div class="texto-servicios">
+    <div class="texto-servicios vermas">
         <!-- <blockquote>
         Telecomunicaciones y redes
         </blockquote> -->
         <p>Soluciones de ingeniería en telecomunicaciones y redes. Nuestro equipo de expertos altamente capacitados se dedica a brindar servicios de calidad a empresas y particulares, ayudándoles a alcanzar una conectividad confiable y eficiente. </p>
 
         <p>Creamos soluciones personalizadas de redes que se adaptan a tus necesidades específicas. Ya sea que busques implementar una red local, una red de área amplia (WAN) o una red inalámbrica, nuestro equipo se encargará de diseñar e implementar una infraestructura sólida y segura.</p>
-        <nav class="navegacion">
-            <a href="/telecomunicaciones">Ver mas</a> 
+
+            <a href="/telecomunicaciones">Ver mas -></a> 
                               
-            </nav>
+       
         <!-- <a href="/informatica" class="boton-amarillo-block">
                Ver mas
             </a> -->
@@ -73,17 +74,17 @@ Redes y comunicaciones.</p>
             </picture>
         </div>
 
-        <div class="texto-servicios">
+        <div class="texto-servicios vermas">
             <!-- <blockquote>
             Informáticos
             </blockquote> -->
             <p>Brindar soluciones de automatización para empresas y particulares. Nuestro objetivo es ayudar a nuestros clientes a optimizar sus procesos, reducir costos y mejorar su eficiencia a través de la implementación de tecnologías avanzadas.</p>
 
             <p>Nuestro equipo de expertos altamente capacitados trabaja estrechamente con los clientes para comprender sus necesidades específicas y diseñar soluciones personalizadas. Ofrecemos una amplia gama de servicios de automatización.</p>
-            <nav class="navegacion">
-            <a href="/automatizacion">Ver mas</a> 
+ 
+            <a href="/automatizacion">Ver mas -></a> 
                               
-            </nav>
+       
             <!-- <a href="/informatica" class="boton-amarillo-block">
                Ver mas
             </a> -->
@@ -101,17 +102,17 @@ Redes y comunicaciones.</p>
             </picture>
         </div>
 
-        <div class="texto-servicios">
+        <div class="texto-servicios vermas">
             <!-- <blockquote>
             Informáticos
             </blockquote> -->
             <p>Nos enorgullece ofrecer soluciones eléctricas de calidad tanto para empresas como para particulares. Nuestra pasión por la excelencia técnica y nuestro compromiso con la satisfacción del cliente nos distinguen en la industria. </p>
 
             <p>Contamos con un equipo de profesionales altamente capacitados para realizar instalaciones eléctricas seguras y eficientes en todo tipo de edificaciones, ya sean residenciales, comerciales o industriales.</p>
-            <nav class="navegacion">
-            <a href="/electricidad">Ver mas</a> 
+            
+            <a href="/electricidad">Ver mas -></a> 
                               
-            </nav>
+           
         <!-- <a href="/informatica" class="boton-amarillo-block">
                Ver mas
             </a> -->
